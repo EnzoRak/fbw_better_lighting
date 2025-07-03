b6e153776fcde17756caf778b3b4abf60e981659
EnzoRak <enzorak@mail.com>
2025-07-03 19:59:08 +0400

    added debugging and fixed bug in ray

---
+++ Game/debugging.lua
@@ -0,0 +1,28 @@
+function p.tick()
+    win_hud.add_element(170, function(wid)
+        ga_win_set_char_size(wid, 0.01, 0.02)
+        ga_win_set_front_color_default(wid)
+        -- casting a ray.
+        local r = ray.c(ga_get_sys_v("game.player.camera.look"))
+        r:cast()
+        local str = "Ray didnt hit"
+        if r.hit then
+            str = "bp: " .. std.vec_to_str(r.bp) .. " normal: " .. std.vec_to_str(r.normal)
+        end
+        ga_win_txt(wid, 0.3, 0.3, str)
+        str = "didnt hit"
+        if ga_look_object_block_exists() then
+            local chunk_id = ga_look_object_block_get_chunk_id()
+            if ga_chunk_id_to_level(chunk_id) == ga_get_viewer_level() then
+                local bp = ga_chunk_id_and_lbp_to_bp(chunk_id, ga_look_object_block_get_lbp())
+                local normal = std.side_int_to_vec(ga_look_object_block_get_normal_side())
+                str = "bp: " .. std.vec_to_str(bp) .. " normal: " .. std.vec_to_str(normal)
+            end
+        end
+        ga_win_txt(wid, 0.3, 0.4, str)
+    end)
+end
+
+function p.__update_discrete_pre()
+    p.tick()
+end
--- Game/ray.lua
+++ Game/ray.lua
@@ -2,24 +2,25 @@ p.c = obj.class("Ray", nil, {
     new = function(self, look, len)
         self.origin = ga_get_viewer_offset()
         self.level = ga_get_viewer_level()
-        self.dir = look
+        self.look = look
         self.len = len or 256
         return self
     end,
     cast = function(self)
-        local function c(l) ga_vis_test_level(self.level, self.origin, std.vec_add(self.origin, std.vec_scale(self.look, l))) end
+        local function c(l) return ga_vis_test_level(self.level, self.origin, std.vec_add(self.origin, std.vec_scale(self.look, l))) end
         local l = self.len
         local l0 = 0.01
         local result = 0
-        if not c(l) then
+        if c(l) then
             result = 1 -- no blocks in the ray's length
         end
         while result == 0 do -- dangerous!
             local mid = c((l0+l)/2)
+            --ga_print("vec length: "..l..", mid: "..mid..", midpoint: "..(l0+l)/2)
             if mid then
-                l = (l0+l)/2
-            else
                 l0 = (l0+l)/2
+            else
+                l = (l0+l)/2
             end
             if l-l0 < 0.01 then
                 result = 2
